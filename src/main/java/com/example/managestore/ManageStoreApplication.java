607129074e1f957a632735482bebc56ffdc2a89b
Nguyen Dinh An <ngdinhan2000@gmail.com>
2023-01-31 09:43:40 +0700

    init project

---
+++ src/main/java/com/example/managestore/ManageStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.managestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManageStoreApplication.class, args);
+    }
+
+}
